131a8fcb88f6afc129d9557267ebf4f19b9eee41
Vishal Raykar <vishalraykar6@gmail.com>
2024-12-31 00:12:10 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 const parent = React.createElement(
     "div",
     {id:"parent", xyz:"abc"},
     [React.createElement(
         "div",
         {id:"child"},
-        [React.createElement("h1",{id:"h1tag"},"I am an H1 tag !"),React.createElement("h2",{id:"h2tag"},"I am an H2 tag !")]
+        [React.createElement("h1",{id:"h1tag"},"I am an H1 tag !"),React.createElement("h2",{id:"h2tag"},"I am an H2 tag React !")]
     ),
     React.createElement(
         "div",
         {id:"child2"},
-        [React.createElement("h1",{id:"h1tag"},"I am an H1 tag !"),React.createElement("h2",{id:"h2tag"},"I am an H2 tag !")]
+        [React.createElement("h1",{id:"h1tag"},"I am an H1 tag !"),React.createElement("h2",{id:"h2tag"},"I am an H2 tag React !")]
     )]
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(parent);
 console.log(parent);
