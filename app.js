afebd31966da9ff7c3c5ff7f8e3cd5ff2ceb92d7
Vishal Raykar <vishalraykar6@gmail.com>
2024-12-31 12:26:10 +0530

    Assignment

--- app.js
+++ app.js
@@ -1,45 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
-
-//transpiled by parcel's babel bundler before it reaches browser/js engine
-
-// React component
-const jsxheading = (<h1 id = "heading" className="head" tabIndex="5">
-    Namaste react heading using jsx
-    </h1>);
-
-// Class based components - Old
+import companyLogo from './Images/logo.jpg';
+import userIcon from './Images/user.png';
 
 // Functional Components - New - Function that returns a react element 
 
-const elem = <span>Sample react element</span>;
-
-
-
-const titleElement = (<h1 className="head">
-    Namaste react title element !
-</h1>);
 
 // Component Composition
-const HeadingComponent2 =  () => (    
-    <div id = "heading">
-     {titleElement}
-     <h1>Namaste react functional component</h1>
+const HeaderComponent =  () => (    
+    <div className="header">
+         <img className="headerimages" src={companyLogo}/>
+         <input type="text" placeholder="Search.."></input>
+         <img className="headerimages" src={userIcon}/>
     </div>
 );
 
-const TitleComponent = () =>(
-    <h1 className="head">
-        {elem}
-        {HeadingComponent2()}
-        <HeadingComponent2/>
-        <HeadingComponent2>Helllowww</HeadingComponent2>
-        Namaste react title component !
-    </h1>
-);
-
-console.log(jsxheading);
 //JSX => React.createElement => Js Object => HTMLElement(render)
 const root = createRoot(document.getElementById("root"));
-root.render(<TitleComponent/>);
+root.render(<HeaderComponent/>);
