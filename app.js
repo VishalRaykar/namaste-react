ccffdd9c2a77ecdbe11f42f268dd1ba5fc240268
Vishal Raykar <vishalraykar6@gmail.com>
2024-12-31 10:24:24 +0530

    Babel

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
-const parent = React.createElement(
-    "div",
-    {id:"parent", xyz:"abc"},
-    [React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement("h1",{id:"h1tag"},"I am an H1 tag !"),React.createElement("h2",{id:"h2tag"},"I am an H2 tag React !")]
-    ),
-    React.createElement(
-        "div",
-        {id:"child2"},
-        [React.createElement("h1",{id:"h1tag"},"I am an H1 tag !"),React.createElement("h2",{id:"h2tag"},"I am an H2 tag React !")]
-    )]
-);
 
+//transpiled by parcel's babel bundler before it reaches browser/js engine
+const jsxheading = (<h1 id = "heading" className="head">
+    Namaste react heading using jsx
+    </h1>);
+console.log(jsxheading);
+//JSX => React.createElement => Js Object => HTMLElement(render)
 const root = createRoot(document.getElementById("root"));
-
-root.render(parent);
-console.log(parent);
+root.render(jsxheading);
