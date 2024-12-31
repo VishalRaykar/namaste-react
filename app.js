c787648efd6831bfcc0b87b61f5b70357ff7e2bb
Vishal Raykar <vishalraykar6@gmail.com>
2024-12-31 10:43:56 +0530

    Component Composition

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
 
 //transpiled by parcel's babel bundler before it reaches browser/js engine
-const jsxheading = (<h1 id = "heading" className="head">
+
+// React component
+const jsxheading = (<h1 id = "heading" className="head" tabIndex="5">
     Namaste react heading using jsx
     </h1>);
+
+// Class based components - Old
+
+// Functional Components - New - Function that returns a react element 
+
+const TitleComponent = () => (
+    <h1 className="head">
+        Namaste react title component !
+    </h1>
+);
+
+// Component Composition
+const HeadingComponent2 =  () => (
+    <div id = "heading">
+     <TitleComponent/>
+     <h1>Namaste react functional component</h1>
+    </div>
+);
+
+
 console.log(jsxheading);
 //JSX => React.createElement => Js Object => HTMLElement(render)
 const root = createRoot(document.getElementById("root"));
-root.render(jsxheading);
+root.render(<HeadingComponent2/>);
